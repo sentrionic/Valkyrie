52168b0ff54e8698e30d0103e48a7159f4f8e9bd
sentrionic <sentrionic@gmail.com>
2021-02-25 11:28:25 +0100

    Fix docs path

--- server/src/main.ts
+++ server/src/main.ts
@@ -52,7 +52,7 @@ async function bootstrap() {
     .build();
 
   const document = SwaggerModule.createDocument(app, options);
-  SwaggerModule.setup('/api', app, document);
+  SwaggerModule.setup('/', app, document);
   await app.listen(process.env.PORT || 4000);
 }
 
