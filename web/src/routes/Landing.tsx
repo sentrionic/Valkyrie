a3bf7d374c2fdedfc328c17ade237bfcd3e98899
sentrionic <sentrionic@gmail.com>
2021-10-07 15:13:42 +0200

    Fix CI

--- web/src/routes/Landing.tsx
+++ web/src/routes/Landing.tsx
@@ -9,7 +9,7 @@ export const Landing: React.FC = () => (
       subtitle="Whether you’re part of a school club,
         gaming group, worldwide art community,
         or just a handful of friends that want to spend time together,
-        Valkyrie makes it easy to talk every day and hang out more often"
+        Valkyrie makes it easy to talk every day and hang out more often."
       image={`${process.env.PUBLIC_URL}/logo.png`}
       ctaText="Get Started"
       ctaLink="/register"
